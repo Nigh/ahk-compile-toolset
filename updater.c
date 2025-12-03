092b2a41c53a0ab99a36c598c30e9297313ed329
xianii <xianyi.xia@outlook.com>
2025-12-03 21:47:58 +0800

    refactor updater.c: update compilation instructions and improve main function argument handling

--- updater.c
+++ updater.c
@@ -1,10 +1,13 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 #include <tchar.h>
 
 // Compile:
-// tcc .\updater.c -DdownloadFilename='"download_Filename"' -DbinaryFilename='"binary_Filename"'
+// tcc .\updater.c -o updater.exe
+// Run:
+// updater.exe download.zip app.exe
 
 void newProcess(const char *path) {
 	STARTUPINFO si = { sizeof(si) };
@@ -17,8 +20,17 @@ void newProcess(const char *path) {
 	}
 }
 
-int main(void) {
-    TCHAR cmd[512];
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+		printf("Usage: updater.exe <download_file> <binary_file>\n");
+		printf("Example: updater.exe app.zip app.exe\n");
+		return 1;
+	}
+
+	const char *downloadFilename = argv[1];
+	const char *binaryFilename = argv[2];
+
+	char cmd[512];
 	sprintf(cmd, "powershell -command \"Expand-Archive -Force %s .\"", downloadFilename);
 	system(cmd);
 	remove(downloadFilename);
